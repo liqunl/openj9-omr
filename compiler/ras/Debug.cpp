0df475ec2654ed2d3e57b4097cc679e7878f7c2c
Liqun Liu <liqunl@ca.ibm.com>
2019-01-25 15:47:24 -0500

    Add helper to report final field modification
    Signed-off-by: Liqun Liu <liqunl@ca.ibm.com>

--- compiler/ras/Debug.cpp
+++ compiler/ras/Debug.cpp
@@ -3918,6 +3918,7 @@ TR_Debug::getRuntimeHelperName(int32_t index)
          case TR_jitResolveStaticFieldDirect: return "jitResolveStaticFieldDirect";
          case TR_jitResolveStaticFieldSetterDirect:  return "jitResolveStaticFieldSetterDirect";
          case TR_reportMethodExit:          return "jitReportMethodExit";
+         case TR_reportFinalFieldModified:  return "jitReportFinalFieldModified";
          case TR_acquireVMAccess:           return "jitAcquireVMAccess";
          case TR_jitCheckIfFinalizeObject:  return "jitCheckIfFinalizeObject";
          case TR_releaseVMAccess:           return "jitReleaseVMAccess";
